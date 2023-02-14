60fdc9001a07896dd529c933c9603e7c92ebc491
rodo-K <rodo.kasidiari@gmail.com>
2023-02-14 18:08:32 +0200

    Add Spring Boot Actuator to monitor and manage the app

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CurrencyConfigurationController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -1,8 +1,6 @@
 package com.in28minutes.springboot.learnspringboot;
 
-import java.util.Arrays;
-import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
