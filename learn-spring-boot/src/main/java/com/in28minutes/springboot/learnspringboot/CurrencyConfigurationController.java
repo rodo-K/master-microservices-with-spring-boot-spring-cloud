ec7d23f43b3f1ae85bde1b5cb5f4bee484f174ea
rodo-K <rodo.kasidiari@gmail.com>
2023-02-14 17:30:53 +0200

    Learn about profiles, logging and different configuration properties

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Simple REST API 
+// http://localhost:8080/courses
+
+@RestController
+public class CurrencyConfigurationController {
+	
+	@Autowired
+	private CurrencyServiceConfiguration configuration;
+	
+	@RequestMapping("/currency-configuration")
+	public CurrencyServiceConfiguration retrieveAllConfiguratio(){
+		return configuration;
+	}
+	
+}
