3dc1bffab6b59ea8fefc29ec9917c24bfc023ffb
rodo-K <rodo.kasidiari@gmail.com>
2023-02-15 17:46:54 +0200

    Create method to add a new user - static list as DB

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -10,19 +12,31 @@ import org.springframework.stereotype.Component;
 public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
 	
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
-
+		users.add(new User(increaseUsersCountByOne(), "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(increaseUsersCountByOne(), "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(increaseUsersCountByOne(), "Jim", LocalDate.now().minusYears(20)));
 	}
-	
+
+		
 	public List<User> findAll(){
 		return users;
 	}
 	
 	
-//	public User save(User user) {}
-//	public User findOne(int id) {}
+	public User save(User user) {
+		user.setId(increaseUsersCountByOne());
+		users.add(user);
+		return user;
+	}
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
+		}
+	
+	private static int increaseUsersCountByOne() {
+		return ++usersCount;
+	}
 	}
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,39 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	private UserDaoService service;
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+
+	//GET /users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	//GET /user/{id}
+	@GetMapping("/users/{id}")
+	public User retrieveUserById(@PathVariable int id){
+		return service.findOne(id);
+	}
+	
+	//POST /users
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user){
+		service.save(user);
+	}
+	
+	
+}
