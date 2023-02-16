78325e0d5db06cfb6fb964fb22d305442946fe0b
rodo-K <rodo.kasidiari@gmail.com>
2023-02-16 16:40:41 +0200

    Implement i18n with message properties - Introduction to versioning

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//		return "Hello World";
 	}
 	
 	@GetMapping(path = "/hello-world-bean")
@@ -22,14 +39,7 @@ public class HelloWorldController {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
-	// So REST API is all about resources and the actions(request methods)
-	// we can perform on those resources
 	
-	// GET: Retrieve detail of a resource
-	// POST: Create a new resource
-	// PUT: Update an existing resource
-	// PATCH: Update part of a resource
-	// DELETE: Delete a resource
 	
 	
 }
