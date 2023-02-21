d36bfb378d0417de6dea0d290d260f919b656048
rodo-K <rodo.kasidiari@gmail.com>
2023-02-21 15:19:37 +0200

    Explore JDBC an JPA

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,8 +1,11 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 
 @Repository
 public class CourseJdbcRepository {
@@ -13,12 +16,34 @@ public class CourseJdbcRepository {
 	private static String INSERT_QUERY = 
 			"""
 			insert into course(id, name, author)
-			values(1, 'Learn AWS', 'in28minutes');
+			values(?, ?, ?);
+
+			""";
+	private static String DELETE_QUERY = 
+			"""
+			delete from course where id=?;
+
+			""";
+	private static String SELECT_QUERY = 
+			"""
+			select * from course where id=?;
 
 			""";
 
-	public void insert() {
-		springJdbcTemlpate.update(INSERT_QUERY);
+	public void insert(Course course) {
+		springJdbcTemlpate.update(INSERT_QUERY,
+				course.getId(), course.getName(), course.getAuthor());
+	}
+	
+	public void deleteById(long id) {
+		springJdbcTemlpate.update(DELETE_QUERY,
+				id);
+	}
+	
+	public Course findById(long id) {
+		return springJdbcTemlpate.queryForObject(SELECT_QUERY,
+				new BeanPropertyRowMapper<>(Course.class),
+				id);
 	}
 
 }
