b6c8c5e7197855590ce23bf30e45a28b7a49e5f6
rodo-K <rodo.kasidiari@gmail.com>
2023-02-21 15:55:21 +0200

    Spring Jpa

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
@@ -12,19 +12,26 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
+	// Entity manager is in the background without calling it 
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
-		repository.insert(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+		repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+		repository.save(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
 		
-		repository.deleteById(1);
+		repository.deleteById(1l);
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByName("Learn DevOps Jpa!"));
 
 
 	}
